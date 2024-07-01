e5c3e4274f9d14d88728226c0a73e47c8529a65c
Takhir <guvalakat1603@gmail.com>
2024-07-01 14:24:04 +0500

    currency exchange service is created

---
+++ currency-exchange-service/src/test/java/uz/tohir/microservices/currency_exchange_service/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.tohir.microservices.currency_exchange_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
