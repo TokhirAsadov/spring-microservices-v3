30f8daa0d3305660d8fa40dee56bfbd793b314e4
Takhir <guvalakat1603@gmail.com>
2024-07-02 13:31:16 +0500

    data are added

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values (10001,'USD','SUM',12600,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values (10002,'EUR','SUM',15740,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values (10003,'AUD','SUM',340,'');
