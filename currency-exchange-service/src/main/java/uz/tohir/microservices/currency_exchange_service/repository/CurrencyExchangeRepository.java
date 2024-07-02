9bb61e8b93b0ec3ee351f88922717f52f6c888c0
Takhir <guvalakat1603@gmail.com>
2024-07-02 13:34:07 +0500

    CurrencyExchangeRepository is created

---
+++ currency-exchange-service/src/main/java/uz/tohir/microservices/currency_exchange_service/repository/CurrencyExchangeRepository.java
@@ -0,0 +1,7 @@
+package uz.tohir.microservices.currency_exchange_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.tohir.microservices.currency_exchange_service.entity.CurrencyExchange;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange,Long> {
+}
