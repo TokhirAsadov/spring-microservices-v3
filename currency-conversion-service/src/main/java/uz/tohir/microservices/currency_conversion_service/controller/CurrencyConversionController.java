556815d2566b2a42d6a28afd9a8a7c1707dc7e67
Takhir <guvalakat1603@gmail.com>
2024-07-02 15:18:13 +0500

    RestTemplate is used

--- currency-conversion-service/src/main/java/uz/tohir/microservices/currency_conversion_service/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/uz/tohir/microservices/currency_conversion_service/controller/CurrencyConversionController.java
@@ -1,12 +1,15 @@
 package uz.tohir.microservices.currency_conversion_service.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import uz.tohir.microservices.currency_conversion_service.entity.CurrencyConversion;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/currency-conversion")
@@ -18,15 +21,26 @@ public class CurrencyConversionController {
             @PathVariable("to") String to,
             @PathVariable("quantity") BigDecimal quantity){
 
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
+                "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                CurrencyConversion.class, uriVariables
+        );
+
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+
 
         return new CurrencyConversion(
-                10001L,
+                currencyConversion.getId(),
                 from,
                 to,
                 quantity,
-                BigDecimal.ONE,
-                BigDecimal.ONE,
-                ""
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment()
         );
     }
 
