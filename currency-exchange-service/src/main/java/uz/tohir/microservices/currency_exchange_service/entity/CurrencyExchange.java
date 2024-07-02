7fbeb85ea14cc1f5f5b4131c796d3c5ce189784d
Takhir <guvalakat1603@gmail.com>
2024-07-02 13:31:29 +0500

    making entity

--- currency-exchange-service/src/main/java/uz/tohir/microservices/currency_exchange_service/entity/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/uz/tohir/microservices/currency_exchange_service/entity/CurrencyExchange.java
@@ -1,11 +1,22 @@
 package uz.tohir.microservices.currency_exchange_service.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 
+@Entity
 public class CurrencyExchange {
+    @Id
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
+
     private BigDecimal conversionMultiple;
     private String environment;
 
