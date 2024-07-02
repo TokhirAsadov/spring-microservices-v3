06586ed6f8599ef7ed213e9aaf5fd18778f75451
Takhir <guvalakat1603@gmail.com>
2024-07-02 13:14:25 +0500

    CurrencyExchangeController is created

---
+++ currency-exchange-service/src/main/java/uz/tohir/microservices/currency_exchange_service/controller/CurrencyExchangeController.java
@@ -0,0 +1,35 @@
+package uz.tohir.microservices.currency_exchange_service.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uz.tohir.microservices.currency_exchange_service.entity.CurrencyExchange;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/currency-exchange")
+public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable("from") String from,
+                                                  @PathVariable("to") String to){
+        CurrencyExchange currencyExchange = new CurrencyExchange(
+                1000L,
+                from,
+                to,
+                BigDecimal.valueOf(50)
+        );
+
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
+    }
+
+}
