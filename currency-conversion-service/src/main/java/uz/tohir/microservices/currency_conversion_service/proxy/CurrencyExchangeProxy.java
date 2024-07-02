868bdb24f72f527743a9e003e493339c0df5c7fb
Takhir <guvalakat1603@gmail.com>
2024-07-02 15:38:29 +0500

    /currency-exchange/from/{from}/to/{to} api is done

--- currency-conversion-service/src/main/java/uz/tohir/microservices/currency_conversion_service/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/uz/tohir/microservices/currency_conversion_service/proxy/CurrencyExchangeProxy.java
@@ -1,10 +1,15 @@
 package uz.tohir.microservices.currency_conversion_service.proxy;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import uz.tohir.microservices.currency_conversion_service.entity.CurrencyConversion;
 
 @FeignClient(name = "currency-conversion-service",url = "localhost:8000")
 public interface CurrencyExchangeProxy {
 
-
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(@PathVariable("from") String from,
+                                                    @PathVariable("to") String to);
 
 }
