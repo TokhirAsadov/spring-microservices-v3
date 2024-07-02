ce8e444010be4e93d96adff0ad7a49007b378491
Takhir <guvalakat1603@gmail.com>
2024-07-02 13:40:34 +0500

    function is created

--- currency-exchange-service/src/main/java/uz/tohir/microservices/currency_exchange_service/repository/CurrencyExchangeRepository.java
+++ currency-exchange-service/src/main/java/uz/tohir/microservices/currency_exchange_service/repository/CurrencyExchangeRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uz.tohir.microservices.currency_exchange_service.entity.CurrencyExchange;
 
 public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange,Long> {
+
+    CurrencyExchange findByFromAndTo(String from, String to);
+
 }
