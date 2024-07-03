274ee595f99e9b15dc09ce17f3665340f21b82da
Takhir <guvalakat1603@gmail.com>
2024-07-03 13:41:17 +0500

    Load Balancer is used that exists in netflix eureka client dependency

--- currency-conversion-service/src/main/java/uz/tohir/microservices/currency_conversion_service/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/uz/tohir/microservices/currency_conversion_service/proxy/CurrencyExchangeProxy.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import uz.tohir.microservices.currency_conversion_service.entity.CurrencyConversion;
 
-@FeignClient(name = "currency-conversion-service",url = "localhost:8000")
+//@FeignClient(name = "currency-exchange-service",url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
