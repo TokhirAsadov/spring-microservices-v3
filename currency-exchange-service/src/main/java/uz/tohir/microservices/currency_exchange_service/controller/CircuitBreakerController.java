e90720bc357dc4482992c29a5c7bb53c8aa60b8c
Takhir <guvalakat1603@gmail.com>
2024-07-04 12:19:44 +0500

    resilience4j.retry.instances.[sample-api].wait-duration=1s
    it provides waiting(1s = 1 second) during every single retrying

---
+++ currency-exchange-service/src/main/java/uz/tohir/microservices/currency_exchange_service/controller/CircuitBreakerController.java
@@ -0,0 +1,27 @@
+package uz.tohir.microservices.currency_exchange_service.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse"/* method name which should exist below*/) //resilience4j.retry.instances.[sample-api].maxAttempts=5
+    public String sampleApi() {
+        logger.info("Sample Api call received");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/circuit-breaker", String.class);
+        return forEntity.getBody(); //"Sample API";
+    }
+
+    public String hardcodedResponse(Exception ex) { // we can also override hardcodedResponse to be able to more flexible another errors sucha as Runtime, NOT_FOUND
+        return "fallback-response by Takhir";
+    }
+}
