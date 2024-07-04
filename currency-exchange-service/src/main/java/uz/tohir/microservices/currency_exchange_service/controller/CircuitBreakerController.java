193fbddebdb05730c477d756f13a2fad30034f45
Takhir <guvalakat1603@gmail.com>
2024-07-04 12:55:54 +0500

    @Bulkhead(name = "default")

--- currency-exchange-service/src/main/java/uz/tohir/microservices/currency_exchange_service/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/uz/tohir/microservices/currency_exchange_service/controller/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package uz.tohir.microservices.currency_exchange_service.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
@@ -17,9 +18,9 @@ public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
     //@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse"/* method name which should exist below*/) //resilience4j.retry.instances.[sample-api].maxAttempts=5
-
-    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse"/* method name which should exist below*/) //resilience4j.retry.instances.[sample-api].maxAttempts=5
-    @RateLimiter(name = "default") // 10s => 10000 calls to the sample-api
+    //@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse"/* method name which should exist below*/) //resilience4j.retry.instances.[sample-api].maxAttempts=5
+    //@RateLimiter(name = "default") // 10s => 10000 calls to the sample-api
+    @Bulkhead(name = "default")
     public String sampleApi() {
         logger.info("Sample Api call received");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/circuit-breaker", String.class);
